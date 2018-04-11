7f75df023c559355a667413a69ae367af1ff7ad0
xsublinialx <34866901+xsublinialx@users.noreply.github.com>
2018-04-11 11:20:41 -0400

    Update installer.py

--- saltz/installer.py
+++ saltz/installer.py
@@ -10,6 +10,7 @@ get_login = getpass.getuser()
 get_dir = os.getcwd()
 greenY = colored("Y")
 redN = colored("n ")
+releases = "\nsaltz-1.0.1-alpha"
 
 areColor = colored("Are you sure you would like to run the installer? ", "red")
 finalAreColor = areColor + greenY + "/" + redN 
@@ -18,6 +19,9 @@ askInstall = raw_input(finalAreColor)
 if askInstall == "Y" or "y":
 	print("Starting install...")
 	newRelease = raw_input("Would you like to install a new release or use a downloaded release? D: Download N: New Release ")
+	if newRelease == "D" or "d":
+		print(releases)
+	
 	sleep(2)
 	os.chdir("~" + get_login)
 	os.system("unzip saltz-1.0.2-alpha.zip")
